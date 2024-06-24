cec085a693a173b3a68b40e73fbb67fcdffa18ac
Muhammad Abdul Majid P <majidppb@gmail.com>
2024-06-24 07:32:46 +0530

    cors

--- programming-1-CRM/backend-django/crm/settings.py
+++ programming-1-CRM/backend-django/crm/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     
     'userauth',
     'customer',
@@ -49,10 +50,11 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',    
 ]
 
 ROOT_URLCONF = 'crm.urls'
@@ -137,3 +139,9 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "https://majidppb.github.io",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
